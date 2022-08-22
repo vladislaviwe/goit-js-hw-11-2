762c7f32e8ea7bb2555332e37a97534aad15d403
vladislaviwe <vladislaviwe@outlook.com>
2022-08-22 17:02:48 +0300

    Update markup.js

--- src/markup.js
+++ src/markup.js
@@ -1,5 +1,5 @@
-export default function cardMarkup(element) {
-  return `
+export default cardMarkup = (element) =>
+`
 <div class="photo-card">
   <a href="${element.largeImageURL}" class="gallery_link">
     <img class="photo-card_img" src="${element.webformatURL}" alt="${element.tags}" height="270px" loading="lazy" />
@@ -23,5 +23,4 @@ export default function cardMarkup(element) {
     </p>
   </div>
 </div>  
-`;
-}
+`
